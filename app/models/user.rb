a05e9531885994754435e646af18180767f6ab13
Taroon29 <k.taro.a01@gmail.com>
2021-03-27 15:28:19 +0900

    バリデーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :encrypted_password, format: {with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/}
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: {/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name, presence: true, format: {/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name_kana, presence: true, format: {/\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true, format: {/\A[ァ-ヶー－]+\z/}
+  validates :birthday, presence: true
 end
