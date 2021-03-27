3eaa9c8d1990b3325b2a8ee8edd55714da470c1d
Taroon29 <k.taro.a01@gmail.com>
2021-03-27 16:59:09 +0900

    エラーメッセージ表示実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :encrypted_password, format: {with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/}
+  validates :password, format: {with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/}
   validates :nickname, presence: true
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
