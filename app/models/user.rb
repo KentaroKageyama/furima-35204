4292ccfd7ccc62520913e0b10ebf9155415126b6
Taroon29 <k.taro.a01@gmail.com>
2021-03-27 15:36:08 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
 
   validates :encrypted_password, format: {with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/}
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: {/\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name, presence: true, format: {/\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name_kana, presence: true, format: {/\A[ァ-ヶー－]+\z/}
-  validates :last_name_kana, presence: true, format: {/\A[ァ-ヶー－]+\z/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
+
 end
