bdbc52b3223ea4f26aae222de8f1ab4a4f6ce37f
Taroon29 <k.taro.a01@gmail.com>
2021-03-28 17:18:15 +0900

    バリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,10 @@ class Item < ApplicationRecord
     with_options numericality: { other_than: 1 , message: "Select" } do
       validates :category_id, :condition_id, :shipping_fee_id, :shipping_add_id, :shipping_day_id
     end
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" },
+                      format: { with: /\A[0-9]+\z/ , message: "Half-width number"}
   end
+
 
   belongs_to :user
   has_one_attached :image
