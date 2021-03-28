f662cc3d7855111bb7920e4035e71e45e050eb2c
Taroon29 <k.taro.a01@gmail.com>
2021-03-28 13:37:27 +0900

    マイグレーション編集・バリデーション実装

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_add
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :name, :description
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :condition_id, :shipping_fee_id, :shipping_add_id, :shipping_day_id
+    end
+    validates :user, foreign_key: true
+  end
+end
