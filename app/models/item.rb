924736fae374098acfd1e0dc8538cc9dc84150f4
Taroon29 <k.taro.a01@gmail.com>
2021-03-28 14:53:40 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
   with_options presence: true do
-    validates :name, :description
+    validates :name, :description, :user_id
     with_options numericality: { other_than: 1 } do
       validates :category_id, :condition_id, :shipping_fee_id, :shipping_add_id, :shipping_day_id
     end
-    validates :user, foreign_key: true
   end
 
   belongs_to :user
