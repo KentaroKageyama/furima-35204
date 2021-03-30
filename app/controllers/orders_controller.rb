52183fc021e76773728656e9fcf19ed2be64191c
Taroon29 <k.taro.a01@gmail.com>
2021-03-30 17:59:34 +0900

    orderコントローラー編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,24 @@
 class OrdersController < ApplicationController
+ 
   def index
     @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new 
   end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:donation_address).permit(:item_id, :zip_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
 end
