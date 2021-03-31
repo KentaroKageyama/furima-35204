c462864b17f71ef1c62ffc33ef1d2e37c8696cda
Taroon29 <k.taro.a01@gmail.com>
2021-03-31 15:49:34 +0900

    テストコード修正

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -3,10 +3,10 @@ class OrderShippingAddress
   attr_accessor :user_id, :item_id, :zip_code, :prefecture_id, :city, :address, :building, :phone_number, :token
 
   with_options presence: true do
-    validates :city, :address, :token
+    validates :city, :address, :user_id, :item_id, :token
     validates :phone_number, format: {with: /\A[0-9]+\z/}, length: { maximum: 11 }
     validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select"}
   end
 
   def save
