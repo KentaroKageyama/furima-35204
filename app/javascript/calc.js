b7978380273e3d4bb369ced132ad9cec11b123fb
Taroon29 <k.taro.a01@gmail.com>
2021-04-02 18:53:35 +0900

    JavaScript有効ページ指定

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,14 +1,16 @@
-function calc (){
-  const price = document.getElementById("item-price");
-  price.addEventListener("input", () => {
-    const tax = Math.floor(price.value * 0.1);
-    const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = tax;
-    
-    const pro = price.value - tax;
-    const profit = document.getElementById("profit");
-    profit.innerHTML = pro;
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  function calc (){
+    const price = document.getElementById("item-price");
+    price.addEventListener("input", () => {
+      const tax = Math.floor(price.value * 0.1);
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = tax;
+      
+      const pro = price.value - tax;
+      const profit = document.getElementById("profit");
+      profit.innerHTML = pro;
 
-  });
+    });
+  }
+  window.addEventListener('load', calc);
 }
-window.addEventListener('load', calc);
