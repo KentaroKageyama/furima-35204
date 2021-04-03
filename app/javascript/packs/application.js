4f4810302d60aaac3c7d75a6803e4c578c01da4f
Taroon29 <k.taro.a01@gmail.com>
2021-04-03 21:19:33 +0900

    Revert "Slick導入 showページ複数枚画像閲覧実装"
    This reverts commit 802cb0fb2ea0457d5c92094c249fd1279b6dc2d6.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// import '@fortawesome/fontawesome-free/js/all';
-
 require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -14,7 +12,7 @@ require("../card")
 require("../preview")
 
 require('jquery')
-require('../slick.js')
+require('../test')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
