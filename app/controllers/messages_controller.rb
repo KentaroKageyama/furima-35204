783b6de5d48b38c94d79ad2ad501d094fe243353
Taroon29 <k.taro.a01@gmail.com>
2021-04-04 16:26:10 +0900

    Action Cableの実装（データ保存まで）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,16 @@
 class MessagesController < ApplicationController
 
   def create
-    binding.pry
-    @message = Message.new(content: params[:message][:content])
+    @message = Message.new(message_params)
+    if @message.save
+      ActionCable.server.broadcast 'message_channel', content: @message
+    end
   end
-  
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
