2334eab512f5e3089e5b1785b36cc067d1606034
Taroon29 <k.taro.a01@gmail.com>
2021-04-05 14:09:59 +0900

    Favoriteデータ保存削除実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+
+  def create
+    favorite = current_user.favorites.build(item_id: params[:item_id])
+    favorite.save
+    redirect_to item_path(params[:item_id])
+  end
+
+  def destroy
+    favorite = Favorite.find_by(item_id: params[:item_id], user_id: current_user.id)
+    favorite.destroy
+    redirect_to item_path(params[:item_id])
+  end
+
 end
