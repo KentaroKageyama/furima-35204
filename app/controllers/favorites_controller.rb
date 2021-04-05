90b4df478be4406964d01a56f7c2a7bbd57042f5
Taroon29 <k.taro.a01@gmail.com>
2021-04-05 17:51:50 +0900

    お気に入りボタンAjax実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
 
   def create
+    @item = Item.find(params[:item_id])
     favorite = current_user.favorites.build(item_id: params[:item_id])
     favorite.save
-    redirect_to item_path(params[:item_id])
   end
 
   def destroy
+    @item = Item.find(params[:item_id])
     favorite = Favorite.find_by(item_id: params[:item_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to item_path(params[:item_id])
   end
 
 end
