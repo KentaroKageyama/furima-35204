4e7c0631901409ec48a9f8a706eb6a29b583d166
Taroon29 <k.taro.a01@gmail.com>
2021-04-07 10:59:12 +0900

    カード情報データベース保存設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+ 
+    customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
+    @card = customer.cards.first
   end
 
 end
